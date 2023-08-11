b533ec02f624481e9dc1fe50fe0deeaae6a792b5
Justin Giancola <justin.giancola@gmail.com>
2023-08-11 23:24:06 +0000

    Use bignums to avoid problems with overflows

--- app.js
+++ app.js
@@ -1,24 +1,29 @@
 function computeFactors() {
-  const num = parseInt(document.getElementById("n").value, 10);
+  const num = BigInt(document.getElementById("n").value);
+
   const factors = primeFactors(num);
   document.getElementById("output").innerText = "Prime Factors: " + factors.join(", ");
 }
 
 function primeFactors(n) {
   const factors = [];
-  while (n % 2 === 0) {
-    factors.push(2);
-    n /= 2;
+
+  while (n % 2n === 0n) {
+    factors.push(2n);
+    n /= 2n;
   }
 
-  for (let i = 3; i <= Math.sqrt(n); i += 2) {
-    while (n % i === 0) {
+  let i = 3n;
+
+  while (i * i <= n) {
+    while (n % i === 0n) {
       factors.push(i);
       n /= i;
     }
+    i += 2n;
   }
 
-  if (n > 2) {
+  if (n > 1n) {
     factors.push(n);
   }
 
